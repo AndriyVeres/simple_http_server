b3ecb9acf56084f8a9f8a5aa7eb32e310a5b6fb1
AndriyVeres <andriy@veres.engineer>
2022-05-08 14:00:19 +0300

    Added code that simple prints HTTP header to console for incoming HTTP requests for local host.

---
+++ src/server.rs
@@ -0,0 +1,42 @@
+use std::io::Read;
+use crate::http::Request;
+use std::net::TcpListener;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
+
+pub struct Server {
+    addr: String,
+} // pub struct Server
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    pub fn run(self) {
+        println!("Listening on {}",self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _))  => {
+
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}",String::from_utf8_lossy(&buffer));
+
+                            Request::try_from(&buffer[..]);
+                            let res: &Result<Request, _> = &buffer[..].try_into();
+                        },
+                        Err(e) => println!("Failed to read from connection: {}",e),
+
+                    }
+                },
+                Err(e) => println!("Failed to establish a connection: {}",e),
+            }
+        }
+    }
+} // impl Server
