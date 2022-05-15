2d08cd63d4c98d51aa996aafff51636f7654bfdf
AndriyVeres <andriy@veres.engineer>
2022-05-15 17:00:45 +0300

    Implemented simple getters for some simple HTTP requests.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@
 use server::Server;
 use http::Request;
 use http::Method;
+use website_handler::WebSiteHandler;
 
 mod http;
 mod server;
+mod website_handler;
 
 fn main () {
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    server.run(WebSiteHandler);
 }
