edb03cf601a93e5d7097f38b01261708c865bc00
AndriyVeres <andriy@veres.engineer>
2022-05-15 16:01:28 +0300

    Added generation of simple responses.

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
-use std::io::Read;
-use crate::http::Request;
+use std::io::{Write, Read};
+use crate::http::{Request, Response, StatusCode};
 use std::net::TcpListener;
 use std::convert::TryFrom;
 use std::convert::TryInto;
@@ -22,21 +22,28 @@ impl Server {
         loop {
             match listener.accept() {
                 Ok((mut stream, _))  => {
-
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}",String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
+                                    Response::new(
+                                        StatusCode::Ok,
+                                        Some("<h1>IT'S WORKING!</h1>".to_string()),
+                                    )
                                 }
-                                Err(e) => println!("Failed to parse request: {}",e),
+                                Err(e) => {
+                                    println!("Failed to parse request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response :{}",e);
                             }
-
-
-                        },
+                        }
                         Err(e) => println!("Failed to read from connection: {}",e),
 
                     }
